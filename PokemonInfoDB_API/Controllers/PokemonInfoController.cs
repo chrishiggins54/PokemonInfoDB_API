779ed252ab7d721e49627405cf6238f536fad64f
chrishiggins54 <97624771+chrishiggins54@users.noreply.github.com>
2023-05-17 22:11:06 +0100

    Add project files.

---
+++ PokemonInfoDB_API/Controllers/PokemonInfoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PokemonInfoDB_API.Models;
+
+namespace PokemonInfoDB_API.Controllers
+{
+    public class PokemonInfoController : ApiController
+    {
+        [Route("api/PokemonInfo/{pokedexNo}")]
+        public PokemonInfoTbl Get(int pokedexNo)
+        {
+            //create connection to db 
+            using (PokemonInfoDB_ConnectionString db = new PokemonInfoDB_ConnectionString())
+            {
+                PokemonInfoTbl API_result = new PokemonInfoTbl();
+
+                //search for the whole record which matches the serviceTag provided by the clients
+                API_result = db.PokemonInfoTbls.FirstOrDefault(s => s.PokedexNo == pokedexNo);
+
+                if (API_result != null)
+                {
+                    return API_result;
+                }
+                else
+                    return null;
+            }
+        }
+    }
+}
